d4b1a9f2460b9c8761249e56bb70d468670b9e95
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-19 21:49:57 +0300

    Create directory media/images, add static in urls, add root media in settings

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import Seller, Category, Product
+
+
+@admin.register(Seller)
+class DriverAdmin(UserAdmin):
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(Category)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "title", "seller", "price",
+        "in_stock", "created", "updated",
+    )
+    search_fields = ("title", "seller",)
+    list_filter = ("in_stock",)
+
