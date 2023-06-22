562e58492084d946b93a5d4da79b0e4111715797
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-22 15:44:42 +0300

    Create products list filtered by category, add reference for it in navigation-light

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, category_list
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="index"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("category/<int:pk>", category_list, name="category"),
     ]
 
 app_name = "shop"
