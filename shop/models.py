4accabf9dcf9b9a680c876fb80dc29bc604f43c7
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-22 22:42:00 +0300

    Add default image, reduce the size of main text block in product.html

--- shop/models.py
+++ shop/models.py
@@ -30,7 +30,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE, related_name="product_seller")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="product")
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", default="images/default.png")
     price = models.DecimalField(max_digits=5, decimal_places=2)
     in_stock = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
