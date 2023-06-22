a72566c4bc6d9dd8374d333095b949a00db703cc
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-22 15:40:55 +0300

    Create product_detail.html with css

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from shop.models import Product, Category
@@ -8,6 +8,16 @@ def categories(request):
     return {
         "categories": Category.objects.all()
     }
+
+def category_list(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    products = Product.objects.filter(category=category)
+    context = {
+        "category": category,
+        "products": products
+    }
+    return render(request, "shop/category.html", context=context)
+
 
 class ProductListView(generic.ListView):
     model = Product
