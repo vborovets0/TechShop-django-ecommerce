b21ab0cfa245703b3f2f2cad7b3713a55b417149
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-23 10:55:09 +0300

    Add class Basket, initialize session and basket there

---
+++ basket/basket.py
@@ -0,0 +1,9 @@
+
+
+class Basket:
+    def __init__(self, request):
+        self.session = request.session
+        basket = self.session.get("session_key")
+        if "session_key" not in request.session:
+            basket = self.session["session_key"] = {}
+        self.basket = basket
