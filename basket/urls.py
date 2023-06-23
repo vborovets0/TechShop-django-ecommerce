decb0e642e90cdf3967406a237c618f91fd8172e
Volodymyr Borovets <vborovets0@gmail.com>
2023-06-23 10:46:43 +0300

    Add button "basket" in navigation-light.html

--- basket/urls.py
+++ basket/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.basket_summary, name="basket_summery")
+    path("", views.basket_summary, name="basket_summary")
     ]
 
 app_name = "basket"
