755d76d4d3a22148bc2558c0751049b2aa87e2de
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-02 23:11:39 +0300

    Add RegistrationForm in forms.py

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import RegistrationForm
+
+def account_registration(request):
+    if request.usre.is_authenticated:
+        return redirect("/")
+
+    if request.method == "POST":
+        registerForm = RegistrationForm(request.POST)
+        if registerForm.is_valid():
+            user = registerForm.save(commit=False)
+            user.email = registerForm.cleaned_data["email"]
+            user.set_password(registerForm.cleaned_data["password"])
+            user.is_active = False  # user must confirm registration by email
+            user.save()
