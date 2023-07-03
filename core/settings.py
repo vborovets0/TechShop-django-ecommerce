d8539c2e57c2c1a4bc0a805cc9b1ff6d20071c79
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-03 23:44:32 +0300

    Add EMAIL_BACKEND in settings

--- core/settings.py
+++ core/settings.py
@@ -138,3 +138,6 @@ LOGIN_URL = '/account/login/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Email setting
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
