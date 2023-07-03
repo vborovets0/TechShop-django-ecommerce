7f239f5c1ca79cec40a67ae1c70fa8aba5e908b4
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-03 23:25:57 +0300

    Finish def account_registration in views

--- account/views.py
+++ account/views.py
@@ -1,16 +1,36 @@
+from django.contrib.sites.shortcuts import get_current_site
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from .forms import RegistrationForm
+from .token import account_activation_token
+
 
 def account_registration(request):
-    if request.usre.is_authenticated:
-        return redirect("/")
+    # if request.usre.is_authenticated:
+    #     return redirect("/")
 
-    if request.method == "POST":
+    if request.method == 'POST':
         registerForm = RegistrationForm(request.POST)
         if registerForm.is_valid():
             user = registerForm.save(commit=False)
-            user.email = registerForm.cleaned_data["email"]
-            user.set_password(registerForm.cleaned_data["password"])
-            user.is_active = False  # user must confirm registration by email
+            user.email = registerForm.cleaned_data['email']
+            user.set_password(registerForm.cleaned_data['password'])
+            user.is_active = False
             user.save()
+            current_site = get_current_site(request)
+            subject = 'Activate your Account'
+            message = render_to_string('account/registration/account_activation_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': account_activation_token.make_token(user),
+            })
+            user.email_user(subject=subject, message=message)
+            return HttpResponse('registered succesfully and activation sent')
+    else:
+        registerForm = RegistrationForm()
+    return render(request, 'account/registration/register.html', {'form': registerForm})
