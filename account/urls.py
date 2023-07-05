452ae8a338a4aef6d479e0b2d20383c83b1d96b7
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-06 00:03:11 +0300

    Add defs "account_activate", "dashboard" in views and path in urls

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("register/", views.account_registration, name="register")
-
+urlpatterns = [
+    path("register/", views.account_registration, name="register"),
+    path('activate/<slug:uidb64>/<slug:token>/', views.account_activate, name='activate'),
+    # User dashboard
+    path('dashboard/', views.dashboard, name='dashboard'),
 
 ]
 
--- account/views.py
+++ account/views.py
@@ -1,12 +1,23 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.template.loader import render_to_string
-from django.utils.encoding import force_bytes
-from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 from .forms import RegistrationForm
+from .models import UserBase
 from .token import account_activation_token
+
+@login_required
+@login_required
+def dashboard(request):
+    # orders = user_orders(request)
+    return render(request,
+                  'account/user/dashboard.html',
+                  )
 
 
 def account_registration(request):
@@ -34,3 +45,18 @@ def account_registration(request):
     else:
         registerForm = RegistrationForm()
     return render(request, 'account/registration/register.html', {'form': registerForm})
+
+
+def account_activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = UserBase.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, user.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        return redirect('account:dashboard')
+    else:
+        return render(request, 'account/registration/activation_invalid.html')
