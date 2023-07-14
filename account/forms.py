62445a98af36f2c83f1fd2ee352f314ae64c1410
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-14 15:17:26 +0300

    Add login and logout in account

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,21 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
 from .models import UserBase
+
+
+class UserLoginForm(AuthenticationForm):
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control mb-3', 'placeholder': 'Username', 'id': 'login-username'}))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'id': 'login-pwd',
+        }
+    ))
+
 
 class RegistrationForm(forms.ModelForm):
 
