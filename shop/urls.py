a9ae057841ccdb692dddf6eee48c1b12273acb47
Volodymyr Borovets <vborovets0@gmail.com>
2023-07-17 00:08:07 +0300

    Add CRUD for product

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, category_list
+from .views import (
+    ProductListView,
+    ProductDetailView,
+    category_list,
+    ProductCreateView,
+    ProductUpdateView,
+    ProductDeleteView)
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="index"),
-    path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
     path("category/<int:pk>", category_list, name="category"),
+    # CRUD
+    path("product/<int:pk>/", ProductDetailView.as_view(), name="product-detail"),
+    path("product/create/", ProductCreateView.as_view(), name="product-create"),
+    path("product/<int:pk>/update/", ProductUpdateView.as_view(), name="product-update"),
+    path("product/<int:pk>/delete/", ProductDeleteView.as_view(), name="product-delete")
     ]
 
 app_name = "shop"
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
 
 from shop.models import Product, Category
@@ -18,8 +20,28 @@ class ProductListView(generic.ListView):
     model = Product
     context_object_name = "product_list"
     template_name = "shop/product.html"
-    paginate_by = 8
+    paginate_by = 4
 
 
 class ProductDetailView(generic.DetailView):
     model = Product
+
+
+class ProductCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("shop:index")
+    template_name = "shop/product_form.html"
+
+
+class ProductUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Product
+    fields = "__all__"
+    success_url = reverse_lazy("shop:index")
+    template_name = "shop/product_form.html"
+
+
+class ProductDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Product
+    success_url = reverse_lazy("shop:index")
+
